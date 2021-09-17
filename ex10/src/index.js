a25619bc4e975bafe4baed1e98e4b807a5fdb500
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-17 06:44:19 +0200

    Add files via upload

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+   
+   var myFirstName="Alen";
+   var myLastName="Vatres";
+   var myHomeTown="Sarajevo";
+
+
+   return {
+      myFirstName,
+      myLastName,
+      myHomeTown
+   };
+}
+
+console.log(main());
+module.exports=main;
